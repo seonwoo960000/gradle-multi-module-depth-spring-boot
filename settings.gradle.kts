10b157bf11fd1cb5527c3a9c9232284a92cb8081
Kim Seon Woo <seonwoo960000.kim@linecorp.com>
2023-03-25 16:04:30 +0900

    Add source and processor sub modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,10 @@ rootProject.name = "multi-module"
 
 include("module1")
 include("module2")
+include("processor")
+include("sink")
 include("source")
 //
+project(":processor").projectDir = File("stream/processor")
+project(":sink").projectDir = File("stream/sink")
 project(":source").projectDir = File("stream/source")
